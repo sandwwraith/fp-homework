da407d3fc8d0d89ff25d65f647ec5ad120ea8903
Leonid Startsev <sandwwraith@gmail.com>
2017-11-27 03:06:38 +0300

    Small style cleanup

--- hw3/app/Main.hs
+++ hw3/app/Main.hs
@@ -12,14 +12,15 @@ someFunc = "someFunc"
 main :: IO ()
 main = do
     args <- getArgs
-    if null args then do
-        putStrLn "No program specified, running in interactive mode."
-        putStr "Enter expression: "
-        hFlush stdout
-        expr <- fromString <$> getLine
-        res <- parseAndEval expr
-        putStrLn $ "Result: " ++ show res
-    else do
-        let name = (head args)
-        program <- BS.readFile name
-        runProgram_ name program
+    if null args
+        then do
+            putStrLn "No program specified, running in interactive mode."
+            putStr "Enter expression: "
+            hFlush stdout
+            expr <- fromString <$> getLine
+            res <- parseAndEval expr
+            putStrLn $ "Result: " ++ show res
+        else do
+            let name = (head args)
+            program <- BS.readFile name
+            runProgram_ name program
--- hw3/src/Expressions.hs
+++ hw3/src/Expressions.hs
@@ -21,32 +21,40 @@ data Expr
           , op2 :: Expr }
     | Div { op1 :: Expr
           , op2 :: Expr }
-    | Let String Expr Expr
+    | Let String
+          Expr
+          Expr
     deriving (Eq, Show)
 
+data ExpressionError
+    = MissingVariable String
+    | ZeroDivision
+    deriving (Eq, Typeable)
 
-data ExpressionError = MissingVariable String | ZeroDivision deriving (Eq, Typeable)
 instance Show ExpressionError where
     show (MissingVariable x) = "Variable " ++ x ++ " is missing"
     show (ZeroDivision)      = "Division by zero"
+
 instance Exception ExpressionError
 
 type ExprMap = Map.Map String Int
+
 type ExprCtx = ReaderT ExprMap (Either ExpressionError) Int
 
 eval :: (MonadReader ExprMap m, MonadThrow m) => Expr -> m Int
 eval (Lit n) = return n
 eval (Var x) = asks (Map.lookup x) >>= maybe (throwM (MissingVariable x)) return
-eval (Let name value expr) = eval value >>= \val -> local (Map.insert name val) (eval expr)
+eval (Let name value expr) =
+    eval value >>= \val -> local (Map.insert name val) (eval expr)
 eval (Add a b) = liftM2 (+) (eval a) (eval b)
 eval (Mul a b) = liftM2 (*) (eval a) (eval b)
 eval (Sub a b) = liftM2 (-) (eval a) (eval b)
 eval (Div a b) = do
-      a1 <- eval a
-      b1 <- eval b
-      case b1 of
-          0 -> throwM ZeroDivision
-          _ -> return $ a1 `div` b1
+    a1 <- eval a
+    b1 <- eval b
+    case b1 of
+        0 -> throwM ZeroDivision
+        _ -> return $ a1 `div` b1
 
 doEval :: (MonadThrow m) => Expr -> ExprMap -> m Int
 doEval expr vars = runReaderT (eval expr) vars
